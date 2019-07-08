ac08ac49744254663cf9d55f16318dac8816e33d
Francis Chen <fncis.chen@gmail.com>
2019-07-08 14:29:32 -0700

    started with printing python file

--- src/04_printing.py
+++ src/04_printing.py
@@ -4,13 +4,17 @@ how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
-x = 10
+x = 10 
 y = 2.24552
 z = "I like turtles!"
+
+data = (x,y,z)
 
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+
+print("x is %d, y is %d, z is %s" % data)
 
 # Use the 'format' string method to print the same thing
 
