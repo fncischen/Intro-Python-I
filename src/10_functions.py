749c43b34e0f4b44463989f96dc1f21b8d3e1990
Francis Chen <fncis.chen@gmail.com>
2019-07-10 14:57:06 -0700

    refactored code for cleanliness

--- src/10_functions.py
+++ src/10_functions.py
@@ -3,23 +3,19 @@
 # YOUR CODE HERE
 def checkIfEven(num):
 	if num%2 == 0:
-		return true
+		return True
 	else:
-		return false
+		return False
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+if checkIfEven(num):
+	print("Even!")
+else:
+	print("Odd!")
 
-def printIfEven(num):
-	if num%2 == 0:
-		print("Even!")
-	else:
-		print("Odd!")
-
-checkIfEven(num)
-printIfEven(num)
 # YOUR CODE HERE
 
